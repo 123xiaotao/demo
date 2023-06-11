61c738070efa82553c07b45bfa88855e81c9173e
xiaobai <2215024818@qq.com>
2023-06-11 16:37:47 +0800

    hello2

--- src/main/java/com/xiaobai/demo/entity/User.java
+++ src/main/java/com/xiaobai/demo/entity/User.java
@@ -16,7 +16,7 @@ public class User implements Serializable {
     private String name;
     private Integer age;
 
-//123456789
+//0123456789
     //create_time
     @TableField(fill = FieldFill.INSERT)
     private Date createTime;
