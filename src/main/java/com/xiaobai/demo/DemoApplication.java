01323e62a72e4b2e4da25ba95f772666e527bca3
xiaobai <2215024818@qq.com>
2023-06-17 11:13:06 +0800

    测试

--- src/main/java/com/xiaobai/demo/DemoApplication.java
+++ src/main/java/com/xiaobai/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.xiaobai.demo.Mapper")
 @SpringBootApplication
 public class DemoApplication {
-
+//4564
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
